d873d78b5936ee2dd24caaecc61c13de4950317f
Basile_home_ASUS <bazzzzile@gmail.com>
2016-04-18 23:06:15 +0300

    вывод полной пары "автор - композиция", почищен мусор

--- Shazam_parcer.py
+++ Shazam_parcer.py
@@ -1,26 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
-import requests
-from lxml import html, etree
+from lxml import html
 
-html = html.parse(r"E:\!Go\myshazam-history.html")
+
+song_set = set()
+html = html.parse(r"test\myshazam-history.html")
 root = html.getroot()
-print(root.xpath('//tr/td[2]/text()'))
-# html_string = etree.tostring(html)
-# tree = html.fromstring(html_string)
-
-
-"""
-artist
-/html/body/table/tbody/tr[2]/td[2]
-/html/body/table/tbody/tr[3]/td[2]
-/html/body/table/tbody/tr[4]/td[2]
-html body table tbody tr td a
-"""
-
-"""
-title
-/html/body/table/tbody/tr[2]/td[1]/a
-/html/body/table/tbody/tr[3]/td[1]/a
-/html/body/table/tbody/tr[4]/td[1]/a
-"""
+for n in range(len(root.xpath('//tr/td[2]/text()'))):
+    # название исполнителя - название композиции; сетом убираем дубликаты
+    # TODO убрать дефис между названием песни и исполнителем
+    song_set.add((root.xpath('//tr/td[2]/text()')[n] + ' - ' + root.xpath('//tr/td[1]/a/text()')[n]))
+print(len(song_set))
