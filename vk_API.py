09f479c5c403ffe11cdd01a3e0e0c7f1759d187c
lobanov OFFICE <lobanov@innoter.com>
2016-04-19 12:24:06 +0300

    добавлен комментарий

--- vk_API.py
+++ vk_API.py
@@ -3,8 +3,10 @@ import vk
 # TODO проверять, есть ли в списке аудиозаписей такая песня
 # TODO вычислять среднюю длительность песни в результатах поиска и добавлять её (+ ту, что со словами?)
 # 29
-session = vk.Session(access_token='73a05d70abc418a24aaa27946029b311c530971a5e6f6d1d515bf8dd9e98ce27217b244508316df16b4')
+session = vk.Session(access_token='73a05d70abc418a24aaa27946029b311c530971a5e6f6d1d515bf8dd9e98ce27217b244508316df16b429')
 api = vk.API(session)
 # поиск аудио по названию
 print(api.audio.search(q='Beatles'))
 print(api.audio.get())
+
+"audio search test commit"
