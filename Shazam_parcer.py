460a7f3b186015ed30a71d8f9d9b188244c12727
lobanov OFFICE <lobanov@innoter.com>
2016-04-20 16:03:23 +0300

    переработана структура кода (через def)

--- Shazam_parcer.py
+++ Shazam_parcer.py
@@ -2,12 +2,17 @@
 import os
 from lxml import html
 
+in_file = r"test\myshazam-history.html"
 
-song_set = set()
-html = html.parse(r"test\myshazam-history.html")
-root = html.getroot()
-for n in range(len(root.xpath('//tr/td[2]/text()'))):
-    # название исполнителя - название композиции; сетом убираем дубликаты
-    # TODO убрать дефис между названием песни и исполнителем
-    song_set.add((root.xpath('//tr/td[2]/text()')[n] + ' - ' + root.xpath('//tr/td[1]/a/text()')[n]))
-print(len(song_set))
+
+def get_shazams(filename):
+    shaz_song_set = set()
+    html_file = html.parse(filename)
+    root = html_file.getroot()
+    for n in range(len(root.xpath('//tr/td[2]/text()'))):
+        # название исполнителя - название композиции; сетом убираем дубликаты
+        # TODO убрать дефис между названием песни и исполнителем
+        shaz_song_set.add((root.xpath('//tr/td[2]/text()')[n] + ' - ' + root.xpath('//tr/td[1]/a/text()')[n]))
+    return shaz_song_set
+
+print(get_shazams(in_file))
